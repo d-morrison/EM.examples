7bdcee56eb6b593823ada2583cde3e9ef81e6881
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-02-16 15:46:01 -0500

    in progress

--- julia/mclachlan-p17.jl
+++ julia/mclachlan-p17.jl
@@ -1,56 +1,61 @@
 using DataFrames, Distributions, DataFramesMeta
-using Pipe: @pipe
 
-function gen_data(n = 500000, mu = [0, 2], sigma = 1, pi = 0.8)
+function gen_data(;n = 500000, mu = [0, 2], sigma = 1, pZ1 = 0.8)
     data = DataFrame(
         Obs_ID = 1:n,
-        Z = (rand(Uniform(0,1), n) .> pi) .+ 1,
+        Z = (rand(Uniform(0,1), n) .> pZ1) .+ 1,
         )
 
     @transform!(data, :Y = rand(Normal(0, sigma), n) + mu[:Z])    
-    @select!(data, :Y, :Obs_ID)
+    @select!(data, :Obs_ID, :Y)
     @transform!(data, :p_Y_given_Z_1 = pdf.(Normal(mu[1], sigma), :Y))
     @transform!(data, :p_Y_given_Z_2 = pdf.(Normal(mu[2], sigma), :Y))
 
     return data
   end
 
-function fit_model(
+function fit_model!(
     data; 
-    pi_init = 0.5, 
+    pi_hat_0 = 0.5, 
     tolerance = 0.001,
-    max_iterations = 1000)
+    max_iterations = 1000,
+    progress = DataFrame(iter = 1:(max_iterations+1), pi_hat = undef, ll = undef, ll_diff = undef))
 
-    progress = [(pi_hat=pi_init, loglik=0.0)]
+    pi_hat = pi_hat_0
+    E_step!(data, pi_hat)
+    ll = loglik!(data)
+    progress[1,:] = (iter = 0, pi_hat, ll, ll_diff = NaN)
+    
     for i in 1:max_iterations
-        Q = E_step(data, pi_init)
-        mu, sigma, pi = M_step(Y, Q)
-        log_likelihood = sum(logpdf.(MixtureModel(pi, [Normal(mu[1], sigma), Normal(mu[2], sigma)]), Y))
-        push!(progress, (pi_hat=pi, log_likelihood=log_likelihood))
-        if abs(log_likelihood - progress[end-1].log_likelihood) < tolerance
+        pi_hat = M_step(data)
+        E_step!(data, pi_hat)
+        
+        ll_old = ll
+        ll = loglik!(data)
+        ll_diff = ll - ll_old
+        push!(progress, (i, pi_hat, ll, ll_diff))
+        println(i=i, pi_hat = pi_hat, ll =ll, ll_diff = ll_diff)
+        if ll_diff < tolerance
             break
         end
     end
-    return (mu, sigma, pi, progress)
-en
-
-function E_step(Y, mu, sigma, pi)
-    Q = Array{Float64}(undef, length(Y))
-    for i in 1:length(Y)
-        Q[i] = pi * pdf(Normal(mu[1], sigma), Y[i]) /
-               (pi * pdf(Normal(mu[1], sigma), Y[i]) + (1-pi) * pdf(Normal(mu[2], sigma), Y[i]))
-    end
-    return Q
-end
-rs
-function M_step(Y, Q)
-    N1 = sum(Q)
-    N2 = length(Q) - N1
-    mu = [sum(Q .* Y) / N1, sum((1-Q) .* Y) / N2]
-    sigma = sqrt((sum(Q .* (Y - mu[1]).^2) / N1 + sum((1-Q) .* (Y - mu[2]).^2) / N2) / length(Y))
-    pi = N1 / length(Y)
-    return (mu, sigma, pi)
+    return (pi, progress)
 end
 
-Y = gen_data(1000, [5, 10], 2, 0.5)
-@time fit_model(Y, [0, 20], 2, 0.5)
+function E_step!(data, pi_hat)
+    @transform!(data, :pY_Z1 = :p_Y_given_Z_1 .* pi_hat)
+    @transform!(data, :pY_Z2 = :p_Y_given_Z_2 .* (1- pi_hat))
+    @transform!(data, :pY = :pY_Z1 + :pY_Z2)
+    @transform!(data, :pZ1_given_Y = :pY_Z1 ./ :pY)
+end
+
+function M_step(data)
+    mean(data[!, :pZ1_given_Y])
+end
+
+function loglik!(data)
+    sum(log.(data[!, :pY]))
+end
+
+data = gen_data(n = 1000, pZ1 = 0.8)
+@time fit_model!(data)
